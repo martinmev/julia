951d436a45bfacf9223780c71cc30fa5f0374a16
Jacob Quinn <quinn.jacobd@gmail.com>
2015-05-20 15:42:36 -0600

    Further work on Mmap.Array. Added test/mmap.jl to split out mmap tests

--- base/mmap.jl
+++ base/mmap.jl
@@ -14,17 +14,15 @@ const SEEK_END = Cint(2)
 #
 # Note: a few mappable streams do not support lseek. When Julia
 # supports structures in ccall, switch to fstat.
-function grow!(len::Integer, fd::Integer, offset::FileOffset)
+function grow!(io::IO, offset::FileOffset, len::Integer)
     # Save current file position so we can restore it later
-    cpos = ccall(:jl_lseek, FileOffset, (Cint, FileOffset, Cint), fd, 0, SEEK_CUR)
-    systemerror("lseek", cpos < 0)
-    filelen = ccall(:jl_lseek, FileOffset, (Cint, FileOffset, Cint), fd, 0, SEEK_END)
-    systemerror("lseek", filelen < 0)
-    if (filelen < offset + len)
-        systemerror("pwrite", ccall(:jl_pwrite, Cssize_t, (Cint, Ptr{Void}, UInt, FileOffset), fd, Int8[0], 1, offset + len - 1) < 1)
+    pos = position(io)
+    filelen = filesize(io)
+    if filelen < offset + len
+        write(io, zeros(UInt8,(offset + len) - filelen))
+        flush(io)
     end
-    cpos = ccall(:jl_lseek, FileOffset, (Cint, FileOffset, Cint), fd, cpos, SEEK_SET)
-    systemerror("lseek", cpos < 0)
+    seek(io, pos)
     return
 end
 
@@ -85,36 +83,35 @@ end # @windows_only
 
 # core impelementation of mmap
 type Array{T,N} <: AbstractArray{T,N}
-    array::Base.Array{T,N} # array of memory-mapped data
-    ptr::Ptr{Void}         # pointer to start of memory-mapped data
+    array::Base.Array{T,N} # array of memory-mapped data; *May only refer to `ptr+offset:ptr+length(array)`
+    ptr::Ptr{Void}         # pointer to start of memory-mapped data, points to start of a page boundary
     handle::Ptr{Void}      # only needed on windows for file mapping object
     isreadable::Bool
     iswritable::Bool
-    offset::Int64          # used in munmap on unix
 
-    function Mmap`.Array{T,N}(::Type{T}, io::IO, dims::NTuple{N,Integer}=(filesize(io),), offset::Integer=position(io); grow::Bool=true)
+    function Mmap.Array{T,N}(::Type{T}, io::IO, dims::NTuple{N,Integer}=(filesize(io),), offset::Integer=position(io); grow::Bool=true)
         # check inputs
         isopen(io) || throw(ArgumentError("$io must be open to mmap"))
         applicable(fd,io) || throw(ArgumentError("method `fd(::$T)` doesn't exist, unable to mmap $io"))
-        # Check that none of the computations will overflow
+        applicable(filesize,io) || throw(ArgumentError("method `filesize(::$T)` doesn't exist, unable to mmap $io"))
+
         len = prod(dims) * sizeof(T)
-        len >= 0 || throw(ArgumentError("requested size must be ≥ 0, got $len"))
-        ps = Mmap`.pagesize()
+        len > 0 || throw(ArgumentError("requested size must be > 0, got $len"))
+        ps = Mmap.pagesize()
         len < typemax(Int)-ps || throw(ArgumentError("requested size must be < $(typemax(Int)-ps), got $len"))
 
         offset >= 0 || throw(ArgumentError("requested offset must be ≥ 0, got $offset"))
-        offset > filesize(io) && throw(ArgumentError("requested offset is beyond size of file: $offset > $(filesize(io))"))
 
-        # Set the offset to a page boundary
-        offset_page::FileOffset = div(offset,ps) * ps #DIFF
-        delta::FileOffset = FileOffset(offset-offset_page)
-        len_page = delta + len
+        # shift `offset` to start of page boundary
+        offset_page::FileOffset = div(offset, ps) * ps
+        # add (offset - offset_page) to `len` to get total length of memory-mapped region
+        len_page = (offset - offset_page) + len
 
         # platform-specific internals
          @unix_only begin
             file_desc = fd(io)
-            prot, flags, iswrite = Mmap`.settings(file_desc)
-            iswrite && grow && Mmap`.grow!(len, file_desc, offset)
+            prot, flags, iswrite = Mmap.settings(file_desc)
+            iswrite && grow && Mmap.grow!(io, offset, len)
             # mmap the file
             ptr = ccall(:jl_mmap, Ptr{Void}, (Ptr{Void}, Csize_t, Cint, Cint, Cint, FileOffset), C_NULL, len_page, prot, flags, file_desc, offset_page)
             systemerror("memory mapping failed", reinterpret(Int,ptr) == -1)
@@ -125,40 +122,42 @@ type Array{T,N} <: AbstractArray{T,N}
             hdl::Int = gethandle(io)
             name, readonly, create = settings(io)
             szfile = convert(Csize_t, len + offset)
+            readonly && szfile > filesize(io) && throw(ArgumentError("unable to increase file size to $szfile due to read-only permissions"))
             handle = create ? ccall(:CreateFileMappingW, stdcall, Ptr{Void}, (Cptrdiff_t, Ptr{Void}, Cint, Cint, Cint, Cwstring),
-                                    hdl, C_NULL, readonly ? PAGE_READONLY : PAGE_READWRITE, szfile>>32, szfile&typemax(UInt32), name) :
+                                    hdl, C_NULL, readonly ? PAGE_READONLY : PAGE_READWRITE, szfile >> 32, szfile & typemax(UInt32), name) :
                                   ccall(:OpenFileMappingW, stdcall, Ptr{Void}, (Cint, Cint, Cwstring),
                                     readonly ? FILE_MAP_READ : FILE_MAP_WRITE, true, name)
             handle == C_NULL && error("could not create file mapping: $(Base.FormatMessage())")
             ptr = ccall(:MapViewOfFile, stdcall, Ptr{Void}, (Ptr{Void}, Cint, Cint, Cint, Csize_t),
-                            handle, readonly ? FILE_MAP_READ : FILE_MAP_WRITE, offset_page>>32, offset_page&typemax(UInt32), szfile - convert(Csize_t, offset_page))
+                            handle, readonly ? FILE_MAP_READ : FILE_MAP_WRITE, offset_page >> 32, offset_page & typemax(UInt32), (offset - offset_page) + len)
             ptr == C_NULL && error("could not create mapping view: $(Base.FormatMessage())")
-        end # @windows_only 
-        A = pointer_to_array(convert(Ptr{T}, UInt(ptr)+delta), dims)
-        array = new{T,N}(A,ptr,handle,isreadable(io),iswritable(io),len+delta)
+        end # @windows_only
+        # convert mmapped region to Julia Array at `ptr + (offset - offset_page)` since file was mapped at offset_page
+        A = pointer_to_array(convert(Ptr{T}, UInt(ptr) + (offset - offset_page)), dims)
+        array = new{T,N}(A,ptr,handle,isreadable(io),iswritable(io))
         finalizer(array,close)
         return array
     end
 end
 
-Mmap`.Array{T,N}(::Type{T}, file::AbstractString, dims::NTuple{N,Integer}=(filesize(file),), offset::Integer=Int64(0); grow::Bool=true) =
-    open(io->Mmap`.Array(T, io, dims, offset; grow=grow), file, isfile(file) ? "r+" : "w+")
+Mmap.Array{T,N}(::Type{T}, file::AbstractString, dims::NTuple{N,Integer}=(filesize(file),), offset::Integer=Int64(0); grow::Bool=true) =
+    open(io->Mmap.Array(T, io, dims, offset; grow=grow), file, isfile(file) ? "r+" : "w+")
 
 # using default type: UInt8
-Mmap`.Array{N}(io::IO, dims::NTuple{N,Integer}=(filesize(io),), offset::Integer=position(io); grow::Bool=true) =
-    Mmap`.Array(UInt8, io, dims, offset; grow=grow)
-Mmap`.Array{N}(file::AbstractString, dims::NTuple{N,Integer}=(filesize(io),), offset::Integer=Int64(0); grow::Bool=true) =
-    open(io->Mmap`.Array(UInt8, io, dims, offset; grow=grow), file, isfile(file) ? "r+" : "w+")
+Mmap.Array{N}(io::IO, dims::NTuple{N,Integer}=(filesize(io),), offset::Integer=position(io); grow::Bool=true) =
+    Mmap.Array(UInt8, io, dims, offset; grow=grow)
+Mmap.Array{N}(file::AbstractString, dims::NTuple{N,Integer}=(filesize(io),), offset::Integer=Int64(0); grow::Bool=true) =
+    open(io->Mmap.Array(UInt8, io, dims, offset; grow=grow), file, isfile(file) ? "r+" : "w+")
 
 # using a length argument instead of dims
-Mmap`.Array(io::IO, len::Integer=filesize(io), offset::Integer=position(io); grow::Bool=true) =
-    Mmap`.Array(UInt8, io, (len,), offset; grow=grow)
-Mmap`.Array(file::AbstractString, len::Integer=filesize(file), offset::Integer=Int64(0); grow::Bool=true) =
-    open(io->Mmap`.Array(UInt8, io, (len,), offset; grow=grow), file, isfile(file) ? "r+" : "w+")
+Mmap.Array(io::IO, len::Integer=filesize(io), offset::Integer=position(io); grow::Bool=true) =
+    Mmap.Array(UInt8, io, (len,), offset; grow=grow)
+Mmap.Array(file::AbstractString, len::Integer=filesize(file), offset::Integer=Int64(0); grow::Bool=true) =
+    open(io->Mmap.Array(UInt8, io, (len,), offset; grow=grow), file, isfile(file) ? "r+" : "w+")
 
-function Base.close(m::Mmap`.Array)
+function Base.close(m::Mmap.Array)
     m.isreadable = false; m.iswritable = false
-    @unix_only systemerror("munmap", ccall(:munmap,Cint,(Ptr{Void},Int),m.ptr,m.offset) != 0)
+    @unix_only systemerror("munmap", ccall(:munmap,Cint,(Ptr{Void},Int),m.ptr,length(m.array)) != 0)
     @windows_only begin
         status = ccall(:UnmapViewOfFile, stdcall, Cint, (Ptr{Void},), m.ptr)!=0
         status |= ccall(:CloseHandle, stdcall, Cint, (Ptr{Void},), m.handle)!=0
@@ -167,18 +166,18 @@ function Base.close(m::Mmap`.Array)
     return
 end
 
-Base.iswritable(m::Mmap`.Array) = m.iswritable
-Base.isreadable(m::Mmap`.Array) = m.isreadable
+Base.iswritable(m::Mmap.Array) = m.iswritable
+Base.isreadable(m::Mmap.Array) = m.isreadable
 
 # limited Array interface
-Base.length(m::Mmap`.Array) = length(m.array)
-Base.size(m::Mmap`.Array) = size(m.array)
+Base.length(m::Mmap.Array) = length(m.array)
+Base.size(m::Mmap.Array) = size(m.array)
 
 const READERROR  = ArgumentError("mapped-memory is not readable")
-Base.getindex(S::Mmap`.Array) = isreadable(S) ? getindex(S.array) : throw(READERROR)
-Base.getindex(S::Mmap`.Array, I::Real) = isreadable(S) ? getindex(S.array, I) : throw(READERROR)
-Base.getindex(S::Mmap`.Array, I::AbstractArray) = isreadable(S) ? getindex(S.array, I) : throw(READERROR)
-@generated function Base.getindex(S::Mmap`.Array, I::Union(Real,AbstractVector)...)
+Base.getindex(S::Mmap.Array) = isreadable(S) ? getindex(S.array) : throw(READERROR)
+Base.getindex(S::Mmap.Array, I::Real) = isreadable(S) ? getindex(S.array, I) : throw(READERROR)
+Base.getindex(S::Mmap.Array, I::AbstractArray) = isreadable(S) ? getindex(S.array, I) : throw(READERROR)
+@generated function Base.getindex(S::Mmap.Array, I::Union(Real,AbstractVector)...)
     N = length(I)
     Isplat = Expr[:(I[$d]) for d = 1:N]
     quote
@@ -187,10 +186,10 @@ Base.getindex(S::Mmap`.Array, I::AbstractArray) = isreadable(S) ? getindex(S.arr
 end
 
 const WRITEERROR = ArgumentError("mapped-memory is not writable")
-Base.setindex!(S::Mmap`.Array, x) = iswritable(S) ? setindex!(S.array, x) : throw(WRITEERROR)
-Base.setindex!(S::Mmap`.Array, x, I::Real) = iswritable(S) ? setindex!(S.array, x, I) : throw(WRITEERROR)
-Base.setindex!(S::Mmap`.Array, x, I::AbstractArray) = iswritable(S) ? setindex!(S.array, x, I) : throw(WRITEERROR)
-@generated function Base.setindex!(S::Mmap`.Array, x, I::Union(Real,AbstractVector)...)
+Base.setindex!(S::Mmap.Array, x) = iswritable(S) ? setindex!(S.array, x) : throw(WRITEERROR)
+Base.setindex!(S::Mmap.Array, x, I::Real) = iswritable(S) ? setindex!(S.array, x, I) : throw(WRITEERROR)
+Base.setindex!(S::Mmap.Array, x, I::AbstractArray) = iswritable(S) ? setindex!(S.array, x, I) : throw(WRITEERROR)
+@generated function Base.setindex!(S::Mmap.Array, x, I::Union(Real,AbstractVector)...)
     N = length(I)
     Isplat = Expr[:(I[$d]) for d = 1:N]
     quote
@@ -203,7 +202,7 @@ const MS_ASYNC = 1
 const MS_INVALIDATE = 2
 const MS_SYNC = 4
 
-function sync!(m::Mmap`.Array, flags::Integer=MS_SYNC)
+function sync!(m::Mmap.Array, flags::Integer=MS_SYNC)
     @unix_only systemerror("msync", ccall(:msync, Cint, (Ptr{Void}, Csize_t, Cint), pointer(m.array), length(m), flags) != 0)
     @windows_only systemerror("could not FlushViewOfFile: $(Base.FormatMessage())",
                     ccall(:FlushViewOfFile, stdcall, Cint, (Ptr{Void}, Csize_t), pointer(m.array), length(m)) == 0)
--- test/mmap.jl
+++ test/mmap.jl
@@ -1,14 +1,28 @@
-using Base.Test
-reload("/Users/jacobquinn/julia/base/mmap2.jl")
+# using Base.Test
+# reload("/Users/jacobquinn/julia/base/mmap2.jl")
 
 file = tempname()
+s = open(file, "w") do f
+    write(f, "Hello World\n")
+end
+t = "Hello World".data
+@test Mmap2.Array(UInt8, file, (11,1,1)).array == reshape(t,(11,1,1))
+@test Mmap2.Array(UInt8, file, (1,11,1)).array == reshape(t,(1,11,1))
+@test Mmap2.Array(UInt8, file, (1,1,11)).array == reshape(t,(1,1,11))
+@test_throws ArgumentError Mmap2.Array(UInt8, file, (11,0,1)) # 0-dimension
+@test Mmap2.Array(UInt8, file, (11,)).array == t
+@test Mmap2.Array(UInt8, file, (1,11)).array == t'
+@test_throws ArgumentError Mmap2.Array(UInt8, file, (0,12))
+m = Mmap2.Array(UInt8, file, (1,2,1))
+@test m.array == reshape("He".data,(1,2,1))
+close(m)
 # @unix_only @test_throws SystemError Mmap2.Array(file) # requested len=0 on empty file
 # @windows_only @test_throws ErrorException Mmap2.Array(file) # requested len=0 on empty file
 # @unix_only @test_throws SystemError Mmap2.Array(file,0)
 # @windows_only @test_throws ErrorException Mmap2.Array(file,0)
 s = open(f->f,file,"w")
 m = Mmap2.Array(file,12)
-m[:] = "Hello World\n".data
+m.array[:] = "Hello World\n".data
 Mmap2.sync!(m)
 close(m)
 @test open(readall,file) == "Hello World\n"
@@ -18,23 +32,6 @@ close(s)
 @test_throws Base.UVError Mmap2.Array(s) # closed Mmap.Array
 
 @test_throws SystemError Mmap2.Array("")
-
-t = "Hello World".data
-@test Mmap2.Array(UInt8, file, (11,1,1)) == reshape(t,(11,1,1))
-gc(); gc()
-@test Mmap2.Array(UInt8, file, (1,11,1)) == reshape(t,(1,11,1))
-gc(); gc()
-@test Mmap2.Array(UInt8, file, (1,1,11)) == reshape(t,(1,1,11))
-gc(); gc()
-m = Mmap2.Array(UInt8, file, (1,2,1))
-@test m == reshape("He".data,(1,2,1))
-close(m)
-@test_throws SystemError Mmap2.Array(UInt8, file, (11,0,1)) # 0-dimension
-@test Mmap2.Array(UInt8, file, (11,)) == t
-gc(); gc()
-@test Mmap2.Array(UInt8, file, (1,11)) == t'
-gc(); gc()
-@test_throws SystemError Mmap2.Array(UInt8, file, (0,12))
 
 # negative length
 @test_throws ArgumentError Mmap2.Array(file, -1)
@@ -49,33 +46,32 @@ gc(); gc()
 sz = filesize(file)
 m = Mmap2.Array(file, sz+1)
 @test length(m) == sz+1 # test growing
-@test m[end] == 0x00
-sz = filesize(file)
+@test m.array[end] == 0x00
 close(m)
-m = Mmap2.Array(file, 0, sz)
-close(m)
-@test isempty(m)
 sz = filesize(file)
 m = Mmap2.Array(file, 1, sz)
 @test length(m) == 1
-@test m[1] == 0x00
+@test m.array[1] == 0x00
 close(m)
 sz = filesize(file)
-# requesting offset > filesize(file); could we allow this to get a bunch of zeros?
-@test_throws ArgumentError Mmap2.Array(file, 1, sz+1)
+# test where offset is actually > than size of file; file is grown
+m = Mmap2.Array(file, 1, sz+1)
+@test length(m) == 1
+@test m.array[1] == 0x00
+close(m)
 
-# s = open(file, "r")
-# m = Mmap2.Array(s)
-# m[5] = UInt8('x') # currently segfaults on windows, OutOfMemory on OSX
-# m = nothing; gc(); gc()
+s = open(file, "r")
+m = Mmap2.Array(s)
+@test_throws ArgumentError m[5] = UInt8('x') # tries to setindex! on read-only array
+close(m)
 
-s = open(file, "w")
-write(s, "Hello World\n")
-close(s)
+s = open(file, "w") do f
+    write(f, "Hello World\n")
+end
 
 # m = Mmap2.Array(file)
 # open(file,"w") # errors on widnows because file is mmapped?
-s = open(file, "r") # works
+s = open(file, "r")
 m = Mmap2.Array(s)
 close(s)
 close(m)
@@ -83,31 +79,33 @@ m = Mmap2.Array(file)
 s = open(file, "r+") # works
 c = Mmap2.Array(s)
 d = Mmap2.Array(s)
-c[1] = UInt8('J')
+c.array[1] = UInt8('J')
 Mmap2.sync!(c)
 close(s)
-@test m[1] == UInt8('J')
+@test m.array[1] == UInt8('J')
+@test d.array[1] == UInt8('J')
 close(m)
 close(c)
 close(d)
+@test_throws ArgumentError m[1] # try to read from an mmapped-array that has been unmapped
 
-s = open(file, "w")
-write(s, "Hello World\n")
-close(s)
+s = open(file, "w") do f
+    write(f, "Hello World\n")
+end
 
 s = open(file, "r")
 @test isreadonly(s) == true
 c = Mmap2.Array(UInt8, s, (11,))
-@test c == "Hello World".data
+@test c.array == "Hello World".data
 c = Mmap2.Array(UInt8, s, (UInt16(11),))
-@test c == "Hello World".data
+@test c.array == "Hello World".data
 @test_throws ArgumentError Mmap2.Array(UInt8, s, (Int16(-11),))
 @test_throws ArgumentError Mmap2.Array(UInt8, s, (typemax(UInt),))
 close(s)
 s = open(file, "r+")
 @test isreadonly(s) == false
 c = Mmap2.Array(UInt8, s, (11,))
-c[5] = UInt8('x')
+c.array[5] = UInt8('x')
 Mmap2.sync!(c)
 close(s)
 s = open(file, "r")
@@ -117,18 +115,18 @@ close(s)
 close(c)
 
 c = Mmap2.Array(file)
-@test c == "Hellx World\n".data
+@test c.array == "Hellx World\n".data
 close(c)
 c = Mmap2.Array(file, 3)
-@test c == "Hel".data
+@test c.array == "Hel".data
 close(c)
 s = open(file, "r")
 c = Mmap2.Array(s, 6)
-@test c == "Hellx ".data
+@test c.array == "Hellx ".data
 close(s)
 close(c)
 c = Mmap2.Array(file, 5, 6)
-@test c == "World".data
+@test c.array == "World".data
 close(c)
 
 s = open(file, "w")
@@ -139,26 +137,26 @@ close(s)
 m = Mmap2.Array(file)
 t = "Hello World\n"
 for i = 1:12
-    @test m[i] == t.data[i]
+    @test m.array[i] == t.data[i]
 end
 @test_throws BoundsError m[13]
 close(m)
 
 m = Mmap2.Array(file,6)
-@test m[1] == "H".data[1]
-@test m[2] == "e".data[1]
-@test m[3] == "l".data[1]
-@test m[4] == "l".data[1]
-@test m[5] == "o".data[1]
-@test m[6] == " ".data[1]
-@test_throws BoundsError m[7]
+@test m.array[1] == "H".data[1]
+@test m.array[2] == "e".data[1]
+@test m.array[3] == "l".data[1]
+@test m.array[4] == "l".data[1]
+@test m.array[5] == "o".data[1]
+@test m.array[6] == " ".data[1]
+@test_throws BoundsError m.array[7]
 close(m)
 @test_throws ArgumentError m[1] # try to read unmapped-memory
 
 m = Mmap2.Array(file,2,6)
-@test m[1] == "W".data[1]
-@test m[2] == "o".data[1]
-@test_throws BoundsError m[3]
+@test m.array[1] == "W".data[1]
+@test m.array[2] == "o".data[1]
+@test_throws BoundsError m.array[3]
 close(m)
 @test_throws ArgumentError m[1]
 
@@ -174,13 +172,13 @@ s = open(fname)
 m = read(s, Int)
 n = read(s, Int)
 A2 = Mmap2.Array(Int, s, (m,n))
-@test A == A2
+@test A == A2.array
 close(A2)
 seek(s, 0)
 A3 = Mmap2.Array(Int, s, (m,n), convert(FileOffset,2*sizeof(Int)))
-@test A == A3
+@test A == A3.array
 A4 = Mmap2.Array(Int, s, (m,150), convert(FileOffset,(2+150*m)*sizeof(Int)))
-@test A[:, 151:end] == A4
+@test A[:, 151:end] == A4.array
 close(s)
 close(A2); close(A3); close(A4)
 rm(fname)
